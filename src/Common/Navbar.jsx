30c12528952986377c1e9a8d0d3560a11d337ea5
Ebuka Moses <okoyemoses83@gmail.com>
2024-10-09 13:44:57 +0100

    made the navbar responsive

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import "./Common.css"
-import logo from "/images/logo.png"
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./Common.css";
+import logo from "/images/logo.png";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineCancel } from "react-icons/md";
 
 const Navbar = () => {
+const [navbar, setNavbar] = useState(false)
   return (
     <div className="navbar">
-      <img src={logo} className='logo' alt="Lilian Amaka Okocha" />
-      <nav>
+      <img src={logo} className="logo" alt="Lilian Amaka Okocha" />
+      {navbar ? (
+        <MdOutlineCancel
+          className="navbarIcon"
+          onClick={() => setNavbar(false)}
+        />
+      ) : (
+        <GiHamburgerMenu
+          className="navbarIcon"
+          onClick={() => setNavbar(true)}
+        />
+      )}
+
+      <nav className={`${!navbar && "hidden_nav"}`}>
         <Link to="">Home</Link>
         <Link to="">About</Link>
         <Link to="">Gallery</Link>
       </nav>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
